8d951db1ea053fef32a8a5cb6b037390e4857cf4
Phương Duy <duyhp@nhanlucsieuviet.com>
2021-03-23 11:45:40 +0700

    - fix bug test

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="app">
+      <header className="app-header">
+        <img src={logo} className="app-logo" alt="logo" />
         <div>DEMO CICD TEST</div>
       </header>
     </div>
