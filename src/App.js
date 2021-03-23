e214ec46fd6b6dfaba766d9e26e7ac75c028df7f
Phương Duy <duyhp@nhanlucsieuviet.com>
2021-03-23 11:16:47 +0700

    - fix bug

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div>DEMO CICD</div>
+        <div>DEMO CICD TEST</div>
       </header>
     </div>
   )
