2a580b015b2f732a1c56c373ec18aff1596676b1
Phương Duy <duyhp@nhanlucsieuviet.com>
2021-03-30 10:20:12 +0700

    - Drag and Drop

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import logo from "./logo.svg"
-import "./App.css"
+import { useState } from "react"
+import { DragDropContext } from "react-beautiful-dnd"
+import List from "./component/List"
+import "./css/style.css"
 
 function App() {
+  const [data, setData] = useState([
+    {
+      id: 1,
+      name: "BTC/USTD",
+      score: 100,
+    },
+    {
+      id: 2,
+      name: "DOT/USTD",
+      score: 40,
+    },
+  ])
+
+  const onDragEnd = (result) => {
+    if (!result.destination) return
+    if (
+      result.source.droppableId === result.destination.droppableId &&
+      result.source.index === result.destination.index
+    )
+      return
+    const newItems = [...data]
+    const draggedItem = newItems[result.source.index]
+    newItems.splice(result.source.index, 1)
+    newItems.splice(result.destination.index, 0, draggedItem)
+    setData(newItems)
+  }
+
   return (
     <div className="app">
-      <header className="app-header">
-        <img src={logo} className="app-logo" alt="logo" />
-        <div>DEMO CICD</div>
-      </header>
+      <h2>DragDropContext</h2>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <List data={data} />
+      </DragDropContext>
     </div>
   )
 }
---
+++ src/component/ListItem.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react"
+import { Draggable } from "react-beautiful-dnd"
+
+function ListItem(props) {
+  const { item, index } = props
+  return (
+    <Draggable draggableId={"id-" + index} index={index} key={index}>
+      {(provided) => (
+        <div
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          className="list-item"
+        >
+          {item.name} - {item.score}
+        </div>
+      )}
+    </Draggable>
+  )
+}
+
+export default ListItem
