398d55f31f9c341d8d76f6b614ae562e493e9e25
duyhp <duyhp@nhanlucsieuviet.com>
2021-07-11 23:04:09 +0700

    - feature: Facebook Header

--- src/App.js
+++ src/App.js
@@ -1,42 +1,16 @@
-import { useState } from "react"
-import { DragDropContext } from "react-beautiful-dnd"
-import List from "./component/List"
-import "./css/style.css"
+import { Header } from "./component/Headers/Header"
 
 function App() {
-  const [data, setData] = useState([
-    {
-      id: 1,
-      name: "BTC/USTD",
-      score: 100,
-    },
-    {
-      id: 2,
-      name: "DOT/USTD",
-      score: 40,
-    },
-  ])
-
-  const onDragEnd = (result) => {
-    if (!result.destination) return
-    if (
-      result.source.droppableId === result.destination.droppableId &&
-      result.source.index === result.destination.index
-    )
-      return
-    const newItems = [...data]
-    const draggedItem = newItems[result.source.index]
-    newItems.splice(result.source.index, 1)
-    newItems.splice(result.destination.index, 0, draggedItem)
-    setData(newItems)
-  }
-
   return (
     <div className="app">
-      <h2>DragDropContext</h2>
-      <DragDropContext onDragEnd={onDragEnd}>
-        <List data={data} />
-      </DragDropContext>
+      {/* Header */}
+      <Header />
+
+      <main>
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+      </main>
     </div>
   )
 }
---
+++ src/component/Headers/Header.js
@@ -0,0 +1,58 @@
+import {
+  BellIcon,
+  ChatIcon,
+  ChevronDownIcon,
+  HomeIcon,
+  UserGroupIcon,
+  ViewGridIcon,
+} from "@heroicons/react/solid"
+import {
+  FlagIcon,
+  PlayIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline"
+import { HeaderIcon } from "./HeaderIcon"
+
+export const Header = () => {
+  return (
+    <div className="sticky top-8 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
+      {/* Left */}
+      <div className="flex items-center">
+        <img
+          src="https://links.papareact.com/5me"
+          alt=""
+          width={40}
+          height={40}
+        />
+        <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
+          <SearchIcon className="h-6 text-gray-600" />
+          <input
+            type="text"
+            placeholder="Search Facebook"
+            className="hidden md:inline-flex ml-2 items-center bg-transparent outline-none placeholder-gray-500"
+          />
+        </div>
+      </div>
+      {/* Center */}
+      <div className="flex justify-center flex-grow">
+        <div className="flex space-x-6 md:space-x-2">
+          <HeaderIcon active Icon={HomeIcon} />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
+        </div>
+      </div>
+      {/* Right */}
+      <div className="flex items-center sm:space-x-2 justify-end">
+        {/* Profile picture */}
+        <p className="whitespace-nowrap font-semibold pr-3">Phương Duy</p>
+        <ViewGridIcon className="icon" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <ChevronDownIcon className="icon" />
+      </div>
+    </div>
+  )
+}
